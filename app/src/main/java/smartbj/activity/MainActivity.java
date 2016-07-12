2f2ed87d792b680f816fba44959beaa8202e2c57
king87187 <945084779@qq.com>
2016-07-13 00:02:08 +0800

    111
    1

--- app/src/main/java/smartbj/activity/MainActivity.java
+++ app/src/main/java/smartbj/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package smartbj.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.iceblood.dalu.smartbj.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
 
 /**
  * 主页面
@@ -11,12 +12,17 @@ import com.iceblood.dalu.smartbj.R;
  * @author Kevin
  * @date 2015-10-17
  */
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activty_main);
+		setBehindContentView(R.layout.slm_left);
+
+		SlidingMenu slidingMenu = getSlidingMenu();
+		slidingMenu.setBehindOffset(200);
+		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
 	}
 }
